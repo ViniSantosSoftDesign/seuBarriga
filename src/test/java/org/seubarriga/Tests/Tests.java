8bffaa895262e165d666eb683462cb4f31997004
ViniSantosSoftDesign <88507787+ViniSantosSoftDesign@users.noreply.github.com>
2021-08-19 08:35:46 -0300

    validadeAnAccountCreateded

--- src/test/java/org/seubarriga/Tests/Tests.java
+++ src/test/java/org/seubarriga/Tests/Tests.java
@@ -1,9 +1,11 @@
 package org.seubarriga.Tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -35,13 +37,26 @@ public class Tests {
     }*/
 
     @Test
-    public void addAnAccountSuccessfully() {
+    public void validateTheErrorMessageReturedWhenCreateAnAccountWithoutPutName() {
         driver.get("https://seubarriga.wcaquino.me");
         driver.findElement(By.className("dropdown-toggle")).click();
         driver.findElement(By.linkText("Adicionar")).click();
-
+        driver.findElement(By.className("btn-primary")).submit();
+        Assert.assertEquals("Informe o nome da conta", driver.findElement(By.className("alert-danger")).getText());
 
     }
+
+    @Test
+    public void validateMessageReturedWhenCreateAnAccountSuccessfully(){
+        driver.get("https://seubarriga.wcaquino.me");
+        driver.findElement(By.className("dropdown-toggle")).click();
+        driver.findElement(By.linkText("Adicionar")).click();
+        driver.findElement(By.id("nome")).sendKeys("B");
+        driver.findElement(By.className("btn-primary")).submit();
+        Assert.assertEquals("Conta adicionada com sucesso!", driver.findElement(By.className("alert-success")).getText());
+        
+    }
+
 
 
 }
