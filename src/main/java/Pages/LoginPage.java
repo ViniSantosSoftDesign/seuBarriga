585a0f80d9b82baedede039b14a24b864e614978
ViniSantosSoftDesign <88507787+ViniSantosSoftDesign@users.noreply.github.com>
2021-08-23 11:29:01 -0300

    more

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -6,20 +6,39 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage {
 
     private WebDriver driver;
-    private By email = By.id("email");
-    private By loginButton = By.className("btn-primary");
 
+    //We put By in the end of a variable to be ease to track the tipy of it.
+    private By emailBy = By.id("email");
+    private By loginButtonBy = By.className("btn-primary");
+    private By passwordBy = By.id("senha");
+
+
+    //This is a constructor.
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void writeEmailInTheField(){
+    public void writeEmailInTheField(String userEmail) {
+        driver.findElement(emailBy).sendKeys(userEmail);
 
     }
-    public HomePage clickLoginButton(){
-        driver.findElement(loginButton).click();
+
+    public void writePasswordInTheField(String userPassword) {
+        driver.findElement(passwordBy).sendKeys(userPassword);
+    }
+
+    public HomePage clickLoginButton() {
+        driver.findElement(loginButtonBy).click();
         return new HomePage(driver);
     }
+
+    public HomePage loginValidUser(String userEmail, String userPassword) {
+        writeEmailInTheField(userEmail);
+        writePasswordInTheField(userPassword);
+        return new HomePage(driver);
+
+    }
+
 
     public void setupTest() {
         driver = new ChromeDriver();
